76a1c5868901a07d212a1751dbd3e07d2b46153c
w2021915 <w2021915@apps.losrios.edu>
2024-12-03 15:39:11 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -1,1 +1,24 @@
 #include "Engine.h"
+
+void Engine::input()
+{
+}
+void Engine::update(float dtAsSeconds)
+{
+}
+void Engine::draw()
+{
+}
+
+
+
+// The Engine constructor
+Engine::Engine()
+{
+}
+
+/ Run will call all the private functions
+void Engine::run()
+{
+}
+
