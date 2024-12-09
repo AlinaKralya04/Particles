d0cb3d66dddb5f40fda0f284404abc091d210aaa
AlinaKralya04 <alinakralya04@gmail.com>
2024-12-08 20:27:14 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -76,7 +76,7 @@ void Engine::update(float dtAsSeconds)
 {
 	for (int i = 0; i < m_particles.size();)
 	{
-		if (m_particles[i].getTTL() > 0.0) 
+		if (m_particles[i].getTTL() > 0.0)
 		{
 			//check call update on each Particle in the vector whose ttl (time to live) has not expired
 			m_particles[i].update(dtAsSeconds);
@@ -84,21 +84,9 @@ void Engine::update(float dtAsSeconds)
 		}
 		else
 		{
-			m_particles.erase(i);
-			//"Assign the iterator to this return value"?
+			m_particles.erase(m_particles.begin() + i);
 		}
 	}
-	/*This is best done with an iterator-based for-loop
-	Don't automatically increment the iterator for each iteration
-	if getTTL() > 0.0
-	Call update on that Particle
-	Pass in the time differential since the last frame (dt)
-	increment the iterator
-	else
-	erase the element the iterator points to
-	erase returns an iterator that points to the next element after deletion, or end if it is the end of the vector
-	Assign the iterator to this return value
-	Do not increment the iterator (if you do you might increment past the end of the vector after you delete the last element)*/
 }
 
 void Engine::draw()
